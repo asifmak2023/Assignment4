b03cbd0a6fe9182139cfb78c37566c2ba8dd6202
Muhammad Asif <asif.mak2023@gmail.com>
2025-04-02 10:50:41 +0500

    created earthling.py

---
+++ Assignments 01/02 Intermediate/earthling.py
@@ -0,0 +1,8 @@
+def main():
+    
+   earthling = int(input("Enter mass of an object on Earth! :"))
+   weight_on_mars  = earthling * 0.378
+   print(f"Weight on Mars is : {weight_on_mars}", "kg")
+   
+if __name__ == '__main__':
+    main()
